8171681814f055f578cd24d08cf0c0adf030c7c5
Fikriaz <fikri.achmadfauzi77@gmail.com>
2026-06-09 22:19:57 +0700

    fix login test case

--- tests/auth/login.spec.js
+++ tests/auth/login.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 const { getOtp } = require("../../gmail");
-test("login validation", async ({ page }) => {
+test("login using valid username and password ", async ({ page }) => {
   await page.goto("https://dmoneyportal.roadtocareer.net/login");
   await expect(
     page.getByText('Welcome Back')
@@ -38,3 +38,87 @@ test("login validation", async ({ page }) => {
   ).toBeVisible();
 });
 
+test ("Validation login with wrong username and valid password", async ({page}) => {
+  await page.goto('https://dmoneyportal.roadtocareer.net/login');
+
+  await page.getByRole("textbox").first().fill('abcd21@gmail.com');
+  await page.getByRole("textbox").nth(1).fill('testmail123');
+
+  await page.getByRole('button', {
+    name : 'Login →'
+  }).click();
+
+  await page.waitForTimeout(3000);
+  await expect(page.getByText('User not found'))
+  .toBeVisible();
+});
+
+test ("Validation login with valid username and wrong password", async ({page}) => {
+  await page.goto('https://dmoneyportal.roadtocareer.net/login');
+
+  await page.getByRole("textbox").first().fill('freetestemail77@gmail.com');
+  await page.getByRole("textbox").nth(1).fill('ddfsfeaefae');
+
+  await page.getByRole('button', {
+    name : 'Login →'
+  }).click();
+
+  await page.waitForTimeout(3000);
+  await expect(page.getByText('Login failed. Please input correct email/phone number or password.'))
+  .toBeVisible();
+});
+
+test ("Validation login with wrong username and password", async ({page}) => {
+  await page.goto('https://dmoneyportal.roadtocareer.net/login');
+
+  await page.getByRole("textbox").first().fill('abcd21@gmail.com');
+  await page.getByRole("textbox").nth(1).fill('testmzdfdail123');
+
+  await page.getByRole('button', {
+    name : 'Login →'
+  }).click();
+
+  await page.waitForTimeout(3000);
+  await expect(page.getByText('User not found'))
+  .toBeVisible();
+});
+
+test ("Validation login with blank username and password", async ({page}) => {
+  await page.goto('https://dmoneyportal.roadtocareer.net/login');
+
+  await page.getByRole('button', {
+    name : 'Login →'
+  }).click();
+
+  await page.waitForTimeout(3000);
+  await expect(page.getByText('Email/Phone Number and Password cannot be empty.'))
+  .toBeVisible();
+});
+
+test ("Validation login with blank username", async ({page}) => {
+  await page.goto('https://dmoneyportal.roadtocareer.net/login');
+
+//  await page.getByRole("textbox").first().fill('freetestemail77@gmail.com');
+  await page.getByRole("textbox").nth(1).fill('testmail123');
+  await page.getByRole('button', {
+    name : 'Login →'
+  }).click();
+
+  await page.waitForTimeout(3000);
+  await expect(page.getByText('Email/Phone Number and Password cannot be empty.'))
+  .toBeVisible();
+});
+
+test ("Validation login with blank password", async ({page}) => {
+  await page.goto('https://dmoneyportal.roadtocareer.net/login');
+
+  await page.getByRole("textbox").first().fill('freetestemail77@gmail.com');
+ // await page.getByRole("textbox").nth(1).fill('testmail123');
+  await page.getByRole('button', {
+    name : 'Login →'
+  }).click();
+
+  await page.waitForTimeout(3000);
+  await expect(page.getByText('Email/Phone Number and Password cannot be empty.'))
+  .toBeVisible();
+});
